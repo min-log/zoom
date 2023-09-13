d6198a30ac4484d399649af4f7ccf31735737efe
jimin <minlog.0430@gmail.coom>
2023-09-13 22:32:36 +0900

    socket.io 라이브러리를 사용한 채팅 기본 기능 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,51 +1,85 @@
-// 프론트 앤드
-// webSocket 서버와 연결 --- 사용하고 있는 브라우저의 location 정보를 자동으로 가져와서 넣어준다.
+const socket = io(); // 소켓 실행 및 서버 연결 
 
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("#message");
-const nickForm = document.querySelector("#nick");
+// 1. 룸생성 socket io room 기능이 존재
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 
-function makeMessage(type,payload){
-	const msg = {type,payload}
-	return JSON.stringify(msg);
+const room = document.getElementById("room");
+room.hidden = true;
+
+let roomName;
+
+
+function backendDone(msg){ // 백앤드 작업이 끝났음을 알림
+	console.log("backend done");
+	console.log(msg); // back에서 전달 받는 데이터 
 }
 
-const socket = new WebSocket(`ws://${window.location.host}`);
-
-socket.addEventListener("open",()=>{
-	console.log("connected to server O");
-})
-socket.addEventListener("message", (message) => {
-
+function addMessage(message){
+	const ul = room.querySelector("ul");
 	const li = document.createElement("li");
-	li.innerText = message.data;
-	messageList.append(li);
-
-	console.log("new message : ", message.data);
-})
-
-socket.addEventListener("close", () => {
-	console.log("disconnected from th server");
-})
-
-// setTimeout(()=>{
-// 	socket.send("hello from the browser!");
-// },10000);
+	li.innerText = message;
+	ul.appendChild(li);
+}
 
 
-function handleSubmit(event){
+//2. 메시지 전송
+function handleMessageSubmit(event){
 	event.preventDefault();
-	const input = messageForm.querySelector("input");
-	socket.send(makeMessage("new_message",input.value));
+	const input = room.querySelector("#msg input");
+	const value = input.value;
+	socket.emit("new_message", input.value,roomName,()=>{ // 메시지 내용 , 룸 이름
+		addMessage(`me : ${value}`); // 내가 보낸 메시지 내용 화면에 생성
+	});
+	input.value ="";
+}
+
+// 닉네임 생성
+function handleNameSubmit(event){
+	event.preventDefault();
+	const input = room.querySelector("#name input");
+	const value = input.value;
+	socket.emit("nickname", input.value);
 	input.value = "";
+}
+
+
+function showRoom(){
+	welcome.hidden = true;
+	room.hidden = false;
+	const h3 = room.querySelector("h3");
+	h3.innerText = `Room : ${roomName}`;
+
+	// 1. 열린 채팅방에서 메시지 전송 
+	const msgform = room.querySelector("#msg");
+	const nameform = room.querySelector("#name");
+
+	msgform.addEventListener("submit",handleMessageSubmit);
+	nameform.addEventListener("submit",handleNameSubmit);
 
 }
-messageForm.addEventListener("submit", handleSubmit);
 
-function handleNickSubmit(event){
+// 룸 생성
+function handleRoomSubmit(event){
 	event.preventDefault();
-	const input = nickForm.querySelector("input");
-	socket.send(makeMessage("nickname", input.value));
+	const input = form.querySelector("input");
 
+	// emit(send) :
+		// 1 인자 : 이벤트 명을 만들 수 있다.
+		// 2 인자 : 값으로 Object 전달이 가능하다.socket io가 STRING=> OBJECT로 변경 시켜줌
+		// 3-7 인자 : 보내고 싶은 인자를 계속 추가가 가능하다.
+		// 마지막 인자 : 콜백 기능을 사용할 수 있다.  끝나고 실행되는 
+	socket.emit("enter_room", input.value, showRoom);
+	roomName = input.value;
+	input.value = "";
 }
-nickForm.addEventListener("submit",handleNickSubmit);
+form.addEventListener("submit",handleRoomSubmit);
+
+//서버로 부터 전달 받아오는 메시지
+socket.on("welcome",(user)=>{
+	addMessage(`${user} : someone join`);
+});
+socket.on("bye", (user) => {
+	addMessage(`${user} : someone bye`);
+});
+socket.on("new_message", addMessage);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
-import WebSocket from "ws";
+//import WebSocket from "ws";
+import SocketIo from "socket.io";
 import express from "express";
 import { Socket } from "dgram";
 
@@ -12,22 +13,66 @@ app.get("/",(req,res)=>res.render("home"));
 app.get("/*", (req, res) => res.redirect("/")); 
 
 
+
 const handleListen = () => console.log('Listening on http://localhost:3000 or ws://localhost:3000');
 // 두개가 꼭 필요한 사항은 아니고 한 포트에 두개가 존재 하려면 아래와 같이 작성.
-const server = http.createServer(app); // http서버
-const wss = new WebSocket.Server({server}); // webSocket 서버
+const httpServer = http.createServer(app); // http서버
+const ioServer = SocketIo(httpServer); // SocketIo 서버
+ 
 
+ioServer.on("connection", (socket) =>{
+	socket["nickname"] = "Anon";
+	console.log(socket);
+	socket.onAny((event)=>{
+		console.log(`socat event : ${event}`)
+	});
+
+	//1. 방생성
+	socket.on("enter_room", (roomName,done) =>{	
+		console.log(`socket id: ${socket.id}`);	
+		console.log(socket.rooms);
+		//1)  룸 생성
+		socket.join(roomName);
+		//2) 받아온 데이터 처리 후, 프론트 콜백 함수 실행시켜준다.
+			// 다시 전달해주고 싶은 데이터를 넣어 전송도 가능하다. 
+		done(); 
+		//3) 처음 입장시 방안의 모두에게 입장 메시지 전달
+		socket.to(roomName).emit("welcome",socket.nickname);
+	});
+
+	//2. 방 나갈때  
+	socket.on("disconnecting",()=>{	
+		socket.rooms.forEach((room)=>{
+			socket.to(room).emit("bye",socket.nickname);
+		})
+	});
+
+	//3. 메시지
+	socket.on("new_message",(msg,room,done)=>{
+		socket.to(room).emit("new_message",`${socket.nickname} : ${msg}`);
+		done();
+	});
+
+
+	//4. 닉네임 생성
+	socket.on("nickname",(nickname)=>{
+		socket["nickname"] = nickname;
+	});
+
+});
+
+
+
+
+
+/*  // ws 를 사용한 실시간 채팅 기능 구현------------------------------
+const wss = new WebSocket.Server({httpServer}); // webSocket 서버
 const sockets =[];
-
-
 wss.on("connection", (socket)=>{ // socket : 연결된 브라우저 
 	socket["nickname"] = "Anon" ; 
 	console.log("connected to browser");
-
 	sockets.push(socket);
-	
 	socket.on("close",()=>console.log("disconnected from th browser"));
-
 	socket.on("message", (msg)=>{
 		// 서버에서 String 값으로 object 전달 받음. 
 		const message = JSON.parse(msg);
@@ -48,7 +93,11 @@ wss.on("connection", (socket)=>{ // socket : 연결된 브라우저
 	});
 
 	socket.send("hello!!");
-
 });
-server.listen(3000, handleListen);
+*/
+
+
+
+
+httpServer.listen(3000, handleListen);
 
